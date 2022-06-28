b0188268ea187d890c7475c65a46432816ee1d1b
sech444 <59539238+sech444@users.noreply.github.com>
2022-06-28 15:44:31 +0000

    Update coin_prices.py

--- app/routers/coin_prices.py
+++ app/routers/coin_prices.py
@@ -43,7 +43,7 @@ async def index(background_tasks: BackgroundTasks):
         "coin": "BTC",
         "name": "Bitcoin",
         "rate": get_price("bitcoin"),
-        "coin_logo": "assets\/img\/btc.png"
+        "coin_logo": "assets/img/btc.png"
     }
     
 
@@ -78,7 +78,7 @@ async def index_bch(background_tasks: BackgroundTasks):
         "coin": "BCH",
         "name": "Bitcoin Cash",
         "rate": get_price_bch("bitcoin_cash"),
-        "coin_logo": "assets\/img\/bch.png"
+        "coin_logo": "assets/img/bch.png"
     }
     
 
@@ -115,7 +115,7 @@ async def index_eth(background_tasks: BackgroundTasks):
         "coin": "eth",
         "name": "Ethereum",
         "rate": get_ether_coin_Price("ether"),
-        "coin_logo": "assets\/img\/ether.png"
+        "coin_logo": "assets/img/ether.png"
     }
 
 
@@ -150,7 +150,7 @@ async def index_Stellar(background_tasks: BackgroundTasks):
         "coin": "xlm",
         "name": "Stellar",
         "rate": get_xlmu_Price("stellar"),
-        "coin_logo": "assets\/img\/xlm.png"
+        "coin_logo": "assets/img/xlm.png"
         }
     
 
@@ -185,7 +185,7 @@ async def index_ltc(background_tasks: BackgroundTasks):
         "coin": "ltc",
         "name": "Litecoin",
         "rate": get_Ltccoin_Price('litercoin'),
-        "coin_logo": "assets\/img\/ltc.png"
+        "coin_logo": "assets/img/ltc.png"
     }
     
 
@@ -219,7 +219,7 @@ async def index_xrp(background_tasks: BackgroundTasks):
         "coin": "xrp",
         "name": "Proton",
         "rate": get_xrp_price("Ripple"),
-        "coin_logo": "assets\/img\/xrp.png"
+        "coin_logo": "assets/img/xrp.png"
     }
    
     
@@ -262,7 +262,7 @@ async def index_dash(background_tasks: BackgroundTasks):
         "coin": "dash",
         "name": "Dash",
         "rate": float(getDashcoinPrice("crypto_dash")),
-        "coin_logo": "assets\/img\/dash.png"
+        "coin_logo": "assets/img/dash.png"
     } 
     
     
@@ -280,7 +280,7 @@ def bnb_usdt():
         "coin": "BNB",
         "name": "Binance Coin",
         "rate_usdt": priceFloat,
-        "coin_logo": "assets\/img\/bnb.png"
+        "coin_logo": "assets/img/bnb.png"
     }
     except requests.ConnectionError:
         print("Error querying Bitstamp API")  
@@ -298,7 +298,7 @@ def trx_usdt():
         "coin": "TRX",
         "name": "TRON Coin",
         "rate_usdt": float(data['price']),
-        "coin_logo": "assets\/img\/trx.png"
+        "coin_logo": "assets/img/trx.png"
     }
     
 @router.get('/api/v1/btcusdt',tags=["Coin_Price"])
@@ -342,43 +342,43 @@ listData = [
         "coin": "btc",
         "name": "Bitcoin",
         "rate": get_price("bitcoin"),
-        "coin_logo": "assets\/img\/btc.png"
+        "coin_logo": "assets/img/btc.png"
     },
     {
         "coin": "bch",
         "name": "Bitcoin Cash",
         "rate": get_price_bch("bitcoin_cash"),
-        "coin_logo": "assets\/img\/bch.png"
+        "coin_logo": "assets/img/bch.png"
     },
     {
         "coin": "ltc",
         "name": "Litecoin",
         "rate": get_Ltccoin_Price('litercoin'),
-        "coin_logo": "assets\/img\/ltc.png"
+        "coin_logo": "assets/img/ltc.png"
     },
     {
         "coin": "xrp",
         "name": "Ripple",
         "rate": get_xrp_price("Ripple"),
-        "coin_logo": "assets\/img\/xrp.png"
+        "coin_logo": "assets/img/xrp.png"
     },
     {
         "coin": "dash",
         "name": "dash",
         "rate": getDashcoinPrice("crypto_dash"),
-        "coin_logo": "assets\/img\/dash.png"
+        "coin_logo": "assets/img/dash.png"
     },
     {
         "coin": "xlm",
         "name": "Stellar",
         "rate": get_xlmu_Price("stellar"),
-        "coin_logo": "assets\/img\/xlm.png"
+        "coin_logo": "assets/img/xlm.png"
     },
     {
         "coin": "ether",
         "name": "Ethereum",
         "rate": get_ether_coin_Price("ether"),
-        "coin_logo": "assets\/img\/ether.png"
+        "coin_logo": "assets/img/ether.png"
     },
     {
         
