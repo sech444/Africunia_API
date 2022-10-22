4c4d0336e5580e5f9836cf7adb819e3ae5ba6e6b
sech444 <joekofi082@gmail.com>
2022-10-22 12:36:24 +0100

    Update smart_swap.py

--- app/routers/smart_swap.py
+++ app/routers/smart_swap.py
@@ -558,13 +558,16 @@ def get_afcash_exl20(response: Response, token: str = Depends(token_auth_scheme)
                             detail=f"Insufficient exl20_afcash Funds")
     #print("sending890")
     nonce = w3.eth.get_transaction_count(account_1)
+    print(nonce)
+    nonce += 1
+    print(nonce)
     value_to = w3.toWei(value, 'ether')
     try:
         input_balance = Afcash.functions.transfer(account_2, value_to).buildTransaction(
             {
                 'from': account_1,
                 'nonce': nonce ,
-                'gas': 51000,
+                'gas': 510000,
                 'gasPrice': w3.toWei('55', 'gwei'),
             }
         ) 
