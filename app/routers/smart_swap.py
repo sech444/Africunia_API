afac43a41f37831168b3b35cba4edd77eee4adc8
sech444 <joekofi082@gmail.com>
2022-10-22 08:09:51 +0100

    Update smart_swap.py

--- app/routers/smart_swap.py
+++ app/routers/smart_swap.py
@@ -564,7 +564,7 @@ def get_afcash_exl20(response: Response, token: str = Depends(token_auth_scheme)
             {
                 'from': account_1,
                 'nonce': nonce,
-                'gas': 51000,
+                'gas': 5000000,
                 'gasPrice': w3.toWei('55', 'gwei'),
             }
         ) 
